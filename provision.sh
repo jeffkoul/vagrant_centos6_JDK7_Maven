8d46ae816117a1629b426241aae367bba15e87d5
Jeff Koul <jeff.koul@c02nttleg3qj.home>
2016-04-03 12:50:56 -0400

    Inital scripts

---
+++ provision.sh
@@ -0,0 +1,19 @@
+sudo yum -y update
+
+sudo yum -y install vim
+sudo yum -y install nano
+
+sudo yum -y install gcc
+sudo yum -y install ruby
+
+sudo rpm -i /host/pkgs/jdk-7u80-linux-x64.rpm
+sudo cp /vagrant/profile_JDK7.sh /etc/profile.d/
+sudo chown root: /etc/profile.d/profile_JDK7.sh
+source /etc/profile.d/profile_JDK7.sh
+
+tar xvf /host/pkgs/apache-maven-3.3.9-bin.tar
+mv apache-maven-3.3.9 /opt/
+sudo cp /vagrant/profile_Maven.sh /etc/profile.d/
+sudo chown root: /etc/profile.d/profile_Maven.sh
+source /etc/profile.d/profile_Maven.sh
+
